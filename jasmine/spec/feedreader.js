eaee42142c2a4812ce8c285ad3b5000180c61f5d
Jonathan Rivera <Jonfrivera@yahoo.com>
2018-05-09 23:22:02 -0700

    added comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,12 +1,15 @@
 $(function() {
 
+  // suite for the RSS Feeds
   describe('RSS Feeds', function() {
 
+    // test to check if allFeeds is defined and not empty
     it('are defined', function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
 
+    // test to make sure URLs are defined and not empty
     it('is defined and has a url', function() {
       allFeeds.forEach(function(feed) {
         expect(feed.url).toBeDefined();
@@ -14,6 +17,7 @@ $(function() {
       });
     });
 
+    // test to make sure names are defined and not empty
     it('is defined and has a name', function() {
       allFeeds.forEach(function(feed) {
         expect(feed.name).toBeDefined();
@@ -23,12 +27,15 @@ $(function() {
 
   });
 
+  // suite to test the menu functions
   describe('The Menu', function() {
 
+    // test to check if menu is hidden as a default
     it('has menu hidden by default', function() {
       expect($('body').hasClass('menu-hidden')).toBe(true);
     });
 
+    // test to check if the menu is toggled when clicked
     it('menu should toggle visibilty when clicked', function() {
       var menuIcon = $('.menu-icon-link');
 
@@ -43,25 +50,31 @@ $(function() {
 
   });
 
+
+  // suite to check to make sure theres content
   describe('Initial Entries', function() {
 
+    // before each function to call loadFeed
     beforeEach(function(done) {
       loadFeed(0, function() {
         done();
       });
     });
 
+    // test to check if .feed is empty
     it('.feed should not empty after calling loadFeed', function() {
       expect($('.feed').length).not.toBe(0);
     });
 
   });
 
+  // suite to check for a new feed selection
   describe('New Feed Selection', function() {
 
-    var initialContent,
-        changedContent;
+    var initialContent, // variable for initial content
+        changedContent; // variable for the next content loaded
 
+    // before each to store content within variables
     beforeEach(function(done) {
       loadFeed(0, function() {
         initialContent = $('.header-title').html();
@@ -72,6 +85,7 @@ $(function() {
       });
     });
 
+    // test to check if initial content has changed
     it('new feed should change content', function(done) {
       expect(initialContent !== changedContent).toBe(true);
       done();
